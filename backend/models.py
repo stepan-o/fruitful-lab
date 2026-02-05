db75ef3d123b3ef4b5c22d6d6936d188fdf7704b
stepan-o <stepanoskin007@gmail.com>
2026-02-05 10:30:53 -0500

    feat(db): add Alembic migration to add new fields to pinterest_account_stats_monthly (account_name and uploaded_at), unique constraint for account_name and calendar_month

--- backend/models.py
+++ backend/models.py
@@ -8,6 +8,7 @@ from sqlalchemy import (
     Integer,
     String,
     JSON,
+    UniqueConstraint,
 )
 from sqlalchemy.sql import func
 
@@ -43,7 +44,17 @@ class User(Base):
 class PinterestAccountStatsMonthly(Base):
     __tablename__ = "pinterest_account_stats_monthly"
 
+    __table_args__ = (
+        UniqueConstraint(
+            "account_name",
+            "calendar_month",
+            name="uq_pinterest_monthly_account_month"
+        ),
+    )
+
     id = Column(Integer, primary_key=True, index=True)
+
+    account_name = Column(String(255), nullable=False, index=True)
 
     # Month this row represents (you can store e.g. 2025-12-01 for December 2025)
     calendar_month = Column(Date, nullable=False, index=True)
@@ -53,9 +64,15 @@ class PinterestAccountStatsMonthly(Base):
     outbound_clicks = Column(Integer, nullable=False, default=0)
     saves = Column(Integer, nullable=False, default=0)
 
+    uploaded_at = Column(
+        DateTime(timezone=True), server_default=func.now(), nullable=False
+    )
+
+    # When this DB row was first created
     created_at = Column(
         DateTime(timezone=True), server_default=func.now(), nullable=False
     )
+    # when the row was last changed for any reason (system meaning)
     updated_at = Column(
         DateTime(timezone=True),
         server_default=func.now(),

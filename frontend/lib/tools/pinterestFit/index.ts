a35aafdd978644c00a5d2ad3f76bdb44a3fec0b3
Stepan Oskin <32422710+stepan-o@users.noreply.github.com>
2026-05-15 11:11:02 -0400

    Export Pinterest Fit MailerLite helpers

--- frontend/lib/tools/pinterestFit/index.ts
+++ frontend/lib/tools/pinterestFit/index.ts
@@ -1,6 +1,7 @@
 export * from "./config";
 export * from "./copy";
 export * from "./engine";
+export * from "./mailerLite";
 export * from "./questions";
 export * from "./results";
 export * from "./tracking";

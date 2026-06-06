d6c5682768fd4adddacf4f7591c96274ba018c5b
susycid <suanacidd@gmail.com>
2026-06-06 12:25:32 -0400

    Rescue Bloom Whispers dated Pinterest URLs

--- apps/bloom-whispers/functions/_middleware.js
+++ apps/bloom-whispers/functions/_middleware.js
@@ -1,8 +1,20 @@
 export async function onRequest({ request, next }) {
   const url = new URL(request.url);
+  let shouldRedirect = false;
 
   if (url.hostname === "www.bloomwhispers.com") {
     url.hostname = "bloomwhispers.com";
+    shouldRedirect = true;
+  }
+
+  const datedPostMatch = url.pathname.match(/^\/\d{4}\/\d{2}\/\d{2}\/([^/]+)\/?$/);
+
+  if (datedPostMatch) {
+    url.pathname = `/${datedPostMatch[1]}/`;
+    shouldRedirect = true;
+  }
+
+  if (shouldRedirect) {
     return Response.redirect(url.toString(), 301);
   }
 
